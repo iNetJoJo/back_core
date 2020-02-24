3a2653ee7792fff8eb61f4b7e13cd14a4639f3d3
Karlo Bertina <kerlexov@gmail.com>
2020-02-24 06:22:10 +0100

    ssl disabled pq

--- db/db.go
+++ db/db.go
@@ -13,7 +13,7 @@ import (
 
 func New() *gorm.DB {
 	//pg := "host=" +os.Getenv("DB_HOST")+ " port=" +os.Getenv("DB_PORT")+ " user=" +os.Getenv("DB_USER")+ " dbname=" +os.Getenv("DB_NAME")+ " password=" +os.Getenv("DB_PASSWORD")+ ""
-	db, err := gorm.Open("postgres", "host=db port=5432 user=stivi dbname=baza password=popivi")
+	db, err := gorm.Open("postgres", "host=db port=5432 user=stivi dbname=baza password=popivi sslmode=disable")
 	if err != nil {
 		fmt.Println("storage err: ", err)
 	}
