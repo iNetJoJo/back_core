2ca03a8803e95c7a3fe0a01f8ed0137b7cdf7232
Karlo Bertina <kerlexov@gmail.com>
2020-02-24 05:28:33 +0100

    closing db conn

--- db/db.go
+++ db/db.go
@@ -12,18 +12,17 @@ import (
 )
 
 func New() *gorm.DB {
-	pg := "host=" +os.Getenv("DB_HOST")+ " port=" +os.Getenv("DB_PORT")+
-		  " user=" +os.Getenv("DB_USER")+ " dbname=" +os.Getenv("DB_NAME")+ " password=" +os.Getenv("DB_PASSWORD")+ ""
+	pg := "host=" +os.Getenv("DB_HOST")+ " port=" +os.Getenv("DB_PORT")+ " user=" +os.Getenv("DB_USER")+ " dbname=" +os.Getenv("DB_NAME")+ " password=" +os.Getenv("DB_PASSWORD")+ ""
 	db, err := gorm.Open("postgres", pg)
 	if err != nil {
 		fmt.Println("storage err: ", err)
 	}
 
-	defer db.Close()
-
 	return db
 }
-
+func Close(db *gorm.DB) {
+	defer db.Close()
+}
 func TestDB() *gorm.DB {
 	db, err := gorm.Open("sqlite3", "./../realworld_test.db")
 	if err != nil {
--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 	db.AutoMigrate(d)
 
 	us := store.NewUserStore(d)
+	d.Close()
 	//as := store.NewArticleStore(d)
 	h := handler.NewHandler(us)
 	h.Register(v1)
